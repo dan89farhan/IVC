c2e99cfde5bc1c3b1a56dff188a3c8ae3f02e609
dan89farhan <farhan.pixolo@gmail.com>
2020-11-01 02:35:43 +0530

    Set up indian flag

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 
 // Enable live reload for Electron too
 require('electron-reload')(__dirname, {
@@ -15,8 +15,50 @@ function createWindow() {
         }
     })
 
-    win.loadFile('index.html')
-    // win.webContents.openDevTools()
+    win.loadFile('./src/index.html');
+
+
+    const menuTemplate = [
+        {
+            label: 'Files',
+            submenu: [
+                {
+                    label: 'Feedback',
+                    click: () => {
+                        console.log('Feedback Clicked');
+                    }
+                },
+                {
+                    label: 'version IVC 1.1 - 2020 Freeware',
+                    click: () => {
+                        console.log('version IVC 1.1 - 2020 Freeware Clicked');
+                    }
+                },
+                {
+                    label: 'Software developerd by Ranayas',
+                    click: () => {
+                        console.log('Software developerd by Ranayas Clicked');
+                    }
+                },
+                {
+                    label: 'check for update',
+                    click: () => {
+                        console.log('check for update Clicked');
+                    }
+                },
+                {
+                    role:'quit'
+                },
+            ]
+        }
+    ];
+
+    const menu = Menu.buildFromTemplate(menuTemplate)
+    Menu.setApplicationMenu(menu)
+
+    win.setIcon('./src/assets/img/india-flag.jpg');
+
+    win.webContents.openDevTools();
 }
 
 app.whenReady().then(createWindow)
