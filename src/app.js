4074813dd7a26167ad91cd8ea2d52290309f106d
dan89farhan <farhan.pixolo@gmail.com>
2020-11-12 02:54:23 +0530

    modified HTML code

--- src/app.js
+++ src/app.js
@@ -1,16 +1,36 @@
 const electron = require('electron');
 const settings = require('electron-settings');
 var ffmpeg = require('./ffmpeg');
+
+let helper = require('./helper');
+const resolutions = helper.getResolutions();
+const bitrates = helper.getBitrate();
+// console.log(helper);
 // var command = ffmpeg();
 
 
 // console.log('File used for Persisting Data - ' + settings.file());
 
 function convertVideo(data) {
+    console.log(data);
+    let resolutionIndex =  data.resolution;
+    let bitRateIndex =  data.bitRate;
+    let resolution = resolutions[resolutionIndex];
+    let bitrate = bitrates[bitRateIndex];
+
+    return;
+
     let command = ffmpeg(data.inputFile.path)
-        .videoCodec('libx264')
-        .audioCodec('libmp3lame')
-        .size('320x240')
+        .audioCodec('libopus')
+        .audioBitrate(96)
+        .outputOptions([
+            '-hls_time 10',
+            '-hls_playlist_type vod',
+            `-hls_segment_filename %03d.ts`,
+        ])
+        .on('progress', function (progress) {
+            console.log('Processing: ' + progress.percent + '% done')
+        })
         .on('error', function (err) {
             console.log('An error occurred: ' + err.message);
         })
---
+++ src/helper.js
@@ -0,0 +1,42 @@
+/**
+ * 
+ * @author Farhan M.Baig
+ * 
+ * 
+ */
+
+
+//  get Resulations
+
+module.exports.getResolutions = function () {
+    return [
+        {
+            width: 640,
+            height: 360,
+        },
+        {
+            width: 842,
+            height: 640,
+        },
+        {
+            width: 1280,
+            height: 720,
+        },
+
+        {
+            width: 1920,
+            height: 1080,
+        },
+
+
+    ]
+}
+// get Bitrate
+module.exports.getBitrate = function () {
+    return [
+        5000,
+        2800,
+        1400,
+        800
+    ]
+}
