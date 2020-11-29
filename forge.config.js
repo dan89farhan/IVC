2465fe6030d39dcfaae4850f0ce286d4a49c4db3
dan89farhan <farhan.pixolo@gmail.com>
2020-11-30 03:37:13 +0530

    Added github repository

---
+++ forge.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "make_targets": {
+        "win32": [
+            "squirrel"
+        ],
+        "darwin": [
+            "dmg",
+            "zip"
+        ],
+        "linux": [
+            "deb"
+        ]
+    },
+    "electronInstallerDMG": {},
+    "electronInstallerDebian": {},
+    "electronInstallerRedhat": {},
+    "github_repository": {
+        "owner": "dan89farhan",
+        "name": "IVC"
+    },
+    "windowsStoreConfig": {
+        "packageName": "",
+        "name": "electronexample"
+    }
+}
